c97cc4af5c99a16953b1c90c42c49dc23f4ab3c2
Ri Le <ri.le@upstatement.com>
2021-02-14 00:49:51 -0500

    moved chat client to module

---
+++ public/js/ChatlandAudio.js
@@ -0,0 +1,5 @@
+class ChatlandAudio {
+  constructor() {}
+}
+
+export default ChatlandAudio;
--- public/js/ChatlandClient.js
+++ public/js/ChatlandClient.js
@@ -1,8 +1,10 @@
 import { DateTime } from "https://cdn.skypack.dev/luxon";
+import { default as zzfx } from "https://cdn.skypack.dev/zzfx";
 
 class ChatlandClient {
-  constructor(document) {
-    const $chatbox = document.getElementById("chatbox");
+  constructor(socket, document) {
+    console.log(zzfx);
+    this.chatbox = document.getElementById("chatbox");
 
     const newItem = (content) => {
       const item = document.createElement("div");
@@ -11,26 +13,6 @@ class ChatlandClient {
       return item;
     };
 
-    function appendMessage(data) {
-      const { userData, time, text } = data;
-      const { id, nick } = userData;
-      const item = document.createElement("div");
-      item.className = "message";
-
-      const messageText = document.createElement("span");
-      messageText.innerText = nick + ": " + text;
-      messageText.className = "message-text";
-      item.appendChild(messageText);
-
-      const timestamp = document.createElement("span");
-      timestamp.innerText = time;
-      timestamp.className = "timestamp";
-      item.appendChild(timestamp);
-
-      $chatbox.append(item);
-    }
-
-    const socket = io();
     const userData = {
       id: socket.id,
       isRegistered: false,
@@ -38,8 +20,9 @@ class ChatlandClient {
     };
 
     socket.on("connect", () => {
-      $chatbox.appendChild(newItem("Connected to chat server."));
-      $chatbox.appendChild(
+      this.playSound("chat");
+      this.chatbox.appendChild(newItem("Connected to chat server."));
+      this.chatbox.appendChild(
         newItem("Please set your nick with /nick <your nick>")
       );
       socket.emit("joined", userData);
@@ -55,21 +38,21 @@ class ChatlandClient {
       console.log(data);
       console.log(`New message from ${nick} session ${id}`);
       // addNewMessage({ userData, text, time });
-      appendMessage(data);
+      this.appendMessage(data);
     });
 
-    function addNewMessage(data) {
+    const addNewMessage = (data) => {
       const { userData, text, time } = data;
       const { id, nick } = userData;
       const formatted = `${time} ${nick}: ${text}`;
-      $chatbox.appendChild(newItem(formatted));
-    }
+      this.chatbox.appendChild(newItem(formatted));
+    };
 
-    function addPsaMessage(message) {
+    const addPsaMessage = (message) => {
       // const timestamp = DateTime().now().toFormat("HH:mm");
       const formatted = `Chatland System: ${message}`;
-      $chatbox.appendChild(newItem(formatted));
-    }
+      this.chatbox.appendChild(newItem(formatted));
+    };
 
     const form = document.querySelector("#form");
 
@@ -88,6 +71,7 @@ class ChatlandClient {
           );
         }
       } else {
+        this.playSound("chat");
         console.log(`Sending ${text}`);
         const messageData = {
           userData,
@@ -96,14 +80,41 @@ class ChatlandClient {
         };
         socket.emit("message", messageData);
         // addNewMessage(messageData);
-        appendMessage(messageData);
+        this.appendMessage(messageData);
       }
       e.preventDefault();
       document.querySelector("#textInput").value = "";
     });
+  }
 
-    function clearChat() {
-      // document.querySelector(".chat-log")
+  appendMessage(data) {
+    const { userData, time, text } = data;
+    const { id, nick } = userData;
+    const item = document.createElement("div");
+    item.className = "message";
+
+    const messageText = document.createElement("span");
+    messageText.innerText = nick + ": " + text;
+    messageText.className = "message-text";
+    item.appendChild(messageText);
+
+    const timestamp = document.createElement("span");
+    timestamp.innerText = time;
+    timestamp.className = "timestamp";
+    item.appendChild(timestamp);
+
+    this.chatbox.append(item);
+  }
+
+  playSound(sound) {
+    switch (sound) {
+      case "chat": {
+        console.log("sfx");
+        break;
+      }
+      default: {
+        return null;
+      }
     }
   }
 }
